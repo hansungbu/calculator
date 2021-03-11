7f4a30a0d835bc0a6cc336663b17d13b620aef4f
KR-hansungbu <sungbu1996@gmail.com>
2021-03-11 19:37:49 +0900

    계산기

---
+++ 계산기.js
@@ -0,0 +1,14 @@
+function rldlq(han){
+    const b =document.getElementById('rPtks');
+    b.value += han;
+}
+
+function cnffur(){
+    const a = document.getElementById('rPtks');
+    const rkqt=eval(a.value);
+    if(rkqt===undefined){
+        
+    }else{
+        document.getElementById('rPtks').value = rkqt;
+    };
+}
